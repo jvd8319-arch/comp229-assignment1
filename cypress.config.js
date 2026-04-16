b7f66c4869a51afa88a1ba788f028abfabea7cbb
jvd8319-arch <jvd8319@gmail.com>
2026-04-16 15:25:18 -0400

    Cypress Config

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    experimentalStudio: true,
+
+    // ⭐ Increase timeouts to 12 seconds
+    defaultCommandTimeout: 12000,
+    requestTimeout: 12000,
+    responseTimeout: 12000,
+    pageLoadTimeout: 60000,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
